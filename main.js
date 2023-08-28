efe5129f49fef202a411dce25a4dce3f84e72568
ParkSungGyu1 <59018674+ParkSungGyu1@users.noreply.github.com>
2023-08-28 18:13:28 +0900

    tarot

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ let cardData = dataArr[0];
 
 $("#mainDesc").append(
     `<div class="text-center">
-    <img src="https://upload.wikimedia.org/wikipedia/ko/thumb/9/90/RWS_Tarot_00_Fool.jpg/220px-RWS_Tarot_00_Fool.jpg" class="img-fluid" alt="...">
+    <img src="${cardData.imgUrl}" class="img-fluid" alt="...">
   </div>
-  <h1>${cardData.name}</h1>
+  <h1>${cardData.cardName}</h1>
   <p class="lead">${cardData.desc}</p>
   <p class="lead">
     <a href="#" class="btn btn-lg btn-light fw-bold border-white bg-white">타로 전문상담</a>
