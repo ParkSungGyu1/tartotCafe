97778c19da7e0f3cbe37d49b9c68c3b2869f0d36
ParkSungGyu1 <59018674+ParkSungGyu1@users.noreply.github.com>
2023-08-28 17:58:33 +0900

    tarot

---
+++ cards.js
@@ -0,0 +1,8 @@
+export default[
+    {
+        "cardNum" : "0",
+        "cardName" : "THE FOOL",
+        "imgUrl" : "https://upload.wikimedia.org/wikipedia/ko/thumb/9/90/RWS_Tarot_00_Fool.jpg/220px-RWS_Tarot_00_Fool.jpg",
+        "desc" : "The Fool 카드는 새로운 직업, 프로모션, 프로젝트를 의미하며 새로운 사업 그리고 새로운 아이디어 등 창조적 영감과 관련이 깊습니다. 두려움을 버리고 심장이 움직이는 대로 따르세요."
+    }
+]
