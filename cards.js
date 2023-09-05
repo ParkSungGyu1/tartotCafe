186364e7f9f6476e172df7ced9153413b2fcfc76
ParkSungGyu1 <59018674+ParkSungGyu1@users.noreply.github.com>
2023-09-05 14:05:54 +0900

    tarot

--- cards.js
+++ cards.js
@@ -3,12 +3,132 @@ export default[
         "cardNum" : "0",
         "cardName" : "THE FOOL",
         "imgUrl" : "https://mblogthumb-phinf.pstatic.net/20110624_229/itowa2_1308843977777Mlz97_JPEG/00.jpg?type=w2",
-        "desc" : "The Fool 카드는 새로운 직업, 프로모션, 프로젝트를 의미하며 새로운 사업 그리고 새로운 아이디어 등 창조적 영감과 관련이 깊습니다. 두려움을 버리고 심장이 움직이는 대로 따르세요."
+        "desc" : "당신은 낙천적이고 스트레스없는 스타일의 두려움을 모르는 순수하고 어린애같은 타입입니다. 별로 걱정이 없고 그래서 어떤 일이라도 할 수 있는 용기를 가진, 10대의 마음으로 사는 타입이라고 보입니다. 화려한 옷은 탁월한 패션감각을 돋보이게 합니다."
     },
     {
         "cardNum" : "1",
         "cardName" : "THE MAGICIAN",
         "imgUrl" : "https://mblogthumb-phinf.pstatic.net/20110624_62/itowa2_1308843977966VT7VU_JPEG/01.jpg?type=w2",
-        "desc" : "THE MAGICIAN 카드는 마법사 카드의 무궁무진한 가능성과 그의 잠재력. 그는 분명 뛰어난 사람이 될 카드입니다. 하지만 그에게 부족한 것은 이제 막 일을 시작했기에 경험이 부족합니다."
+        "desc" : "당신은 활달하고 결단력이 있으며 쇼맨쉽이 강한 리더형의 인기인입니다. 언변이 능숙해서 다소 과장되게 표현하여 잘못하면 허세꾼으로 보일 수도 있지만 총명하고 유능하며 융통성있는 재주꾼입니다."
     },
+    {
+        "cardNum" : "2",
+        "cardName" : "THE HIGH PRIESTESS",
+        "imgUrl" : "https://mblogthumb-phinf.pstatic.net/20110624_142/itowa2_1308843978159dzsgb_JPEG/02.jpg?type=w2",
+        "desc" : "고도로 발달된 직관을 가진 사람으로 상담이나 조언을 할 수 있는 성향이며 현명하고 지혜롭다. 앉아서 머리를 쓰는 지성미는 있지만 결정장애에 부딪히기도 한다. 고집세고 까칠한 성향이 밉지않은 새침한 아가씨 이미지로 보인다."
+    },
+    {
+        "cardNum" : "3",
+        "cardName" : "THE EMPRESS",
+        "imgUrl" : "https://mblogthumb-phinf.pstatic.net/20110624_1/itowa2_1308843978329IOsMc_JPEG/03.jpg?type=w2",
+        "desc" : "사랑받는거에 익숙한 당신은 책임감과 자존심이 세고 남의 시선도 신경쓰는 편이며 정도 많아 자기 사람은 확실하게 돌보는 모성이 강한 어머니의 성향이 강하죠. 현실적인 면과 지성미도 갖춘 당신은 자기 주장이 너무 세서 간혹 외로워지기도 하지만 사랑받는 존재임에 틀림없죠."
+    },
+    {
+        "cardNum" : "4",
+        "cardName" : "THE EMPEROR",
+        "imgUrl" : "https://mblogthumb-phinf.pstatic.net/20110624_112/itowa2_1308843978493Sb0h7_JPEG/04.jpg?type=w2",
+        "desc" : "현명하고 공평하고 분별력있는 당신은 막강한 리더십을 가진 자존심 강한 독재자. 명령하는 일에 익숙하지만 속마음은 약해 외로움을 삼키는 스타일. 자신의 약한 모습을 숨기다보니 감정표현도 인색하고 타인이 다가서기 힘들죠. 폼에살고 폼에 죽는 완전 남성적 성향."
+    },
+    {
+        "cardNum" : "5",
+        "cardName" : "https://mblogthumb-phinf.pstatic.net/20110624_59/itowa2_1308843978681xEpar_JPEG/05.jpg?type=w2",
+        "imgUrl" : "THE HIEROPHANT",
+        "desc" : "조금은 답답하지만 도덕적이고 바른생활 맨이며 편안한 스타일이다. 말수가 적고 법을 잘 지키는 보수성향이 강한 형식을 중요시하는 그런 사람입니다."
+    },
+    {
+        "cardNum" : "6",
+        "cardName" : "THE LOVERS",
+        "imgUrl" : "https://mblogthumb-phinf.pstatic.net/20110624_166/itowa2_13088439788514BBLJ_JPEG/06.jpg?type=w2",
+        "desc" : "두 사람 사이의 인연을 알려주는 연인 카드의 성향은 질투심은 있지만 싸움을 싫어하고 조화롭기를 원하는 솔직한 성향의 애교쟁이 입니다. 사람을 좋아하고 인정받기를 원하는 자존감 높은 성향의 소유자입니다."
+    },
+    {
+        "cardNum" : "7",
+        "cardName" : "THE CHARIOT",
+        "imgUrl" : "https://mblogthumb-phinf.pstatic.net/20110624_121/itowa2_1308843979005KbK8A_JPEG/07.jpg?type=w2",
+        "desc" : "열정을 가지고 도전하는 혈기왕성한 당신은 지치지않고 도전하는 에너자이저입니다. 두뇌회전이 빠르고 융통성과 순발력을 가진 목표지향적인 사람이죠."
+    },
+    {
+        "cardNum" : "8",
+        "cardName" : "STRENGTH",
+        "imgUrl" : "https://mblogthumb-phinf.pstatic.net/20110624_294/itowa2_13088439791625DN2w_JPEG/08.jpg?type=w2",
+        "desc" : "정신력과 기질이 강해서 본인이 아니라고 생각하면 자기주장대로 살아가는 강한 성격의 소유자입니다. 한번 맺은 인연의 배신이 없고 충성심도 강한편입니다. 불가능을 가능으로 만드는 의지의 스타일입니다."
+    },
+    {
+        "cardNum" : "9",
+        "cardName" : "THE HERMIT",
+        "imgUrl" : "https://mblogthumb-phinf.pstatic.net/20110624_291/itowa2_1308843979360KHl7R_JPEG/09.jpg?type=w2",
+        "desc" : "혼자 있기를 즐기는 당신은 자기성찰과 내면의 등불을 밝히는 사람으로 내면의 성장을 위해 노력하는 성향입니다. 너무 신중해서 활동성이 약한면이 있지만 대인관계는 깊게하는 타입입니다."
+    },
+    {
+        "cardNum" : "10",
+        "cardName" : "WHEEL & FORTUNE",
+        "imgUrl" : "https://mblogthumb-phinf.pstatic.net/20110624_291/itowa2_1308843979776wHzA7_JPEG/10.jpg?type=w2",
+        "desc" : "변화와 삶의 순환을 나타내는 운명의 수레바퀴인 당신의 성향은 다방면에 호기심이 강하고 정보력도 대단합니다. 빠르게 움직이고 싶어하며 경험 욕구가 강해서 이것 저것 해보기를 원하는 타입입니다."
+    },
+    {
+        "cardNum" : "11",
+        "cardName" : "JUSTICE",
+        "imgUrl" : "https://mblogthumb-phinf.pstatic.net/20110624_161/itowa2_1308843980010oyRV5_JPEG/11.jpg?type=w2",
+        "desc" : "너무 깔끔한 성향이라 다소 인간미가 없어 보이지만 분명한 사람이고 모범생 스타일인 당신은 공정함과 명확한 결단력을 지니고 있다. 긍정마인드로 본인의 능력을 최대한 조화롭게 끌어가보기를 권한다."
+    },
+    {
+        "cardNum" : "12",
+        "cardName" : "THE HANGED MAN",
+        "imgUrl" : "https://mblogthumb-phinf.pstatic.net/20110624_98/itowa2_13088439802266pVOM_JPEG/12.jpg?type=w2",
+        "desc" : "욕심이 없다보니 조금 느린 당신은 착하고 선합니다. 원치않는 흐름에 묶여 있는 정체기로 보이지만 정신력은 매우 강하며 다른 사람이 볼 수 없는 혜안과 지혜를 가지고 있죠. 멘탈 최강의 고차원적인 성향이라고 봅니다."
+    },
+    {
+        "cardNum" : "13",
+        "cardName" : "DEATH",
+        "imgUrl" : "https://mblogthumb-phinf.pstatic.net/20110624_245/itowa2_1308843980478SoYCi_JPEG/13.jpg?type=w2",
+        "desc" : "이성이 감성을 다스릴 필요가 있음을 아는 당신은 차갑고 냉정한 성향이 있지만 자존감이 강하고 분명하고 정확한 카리스마있는 성격입니다. 2인자는 안하는 스타일이고 맞다고 생각하면 꼭 해내고야마는 강인함의 끝판왕입니다."
+    },
+    {
+        "cardNum" : "14",
+        "cardName" : "TEMPERANCE",
+        "imgUrl" : "https://mblogthumb-phinf.pstatic.net/20110624_214/itowa2_1308843980666SwXYY_JPEG/14.jpg?type=w2",
+        "desc" : "절제는 억제의 이미지가 떠오르므로 당신의 성격은 참을성이 많고 웬만하면 순리대로 잘 맞춰주는 편안하고 무던한 성향의 소유자입니다. 도덕적이며 믿음이 확 가는 스타일이라 중재자의 역할이 어울린다고 보여집니다."
+    },
+    {
+        "cardNum" : "15",
+        "cardName" : "THE DEVIL",
+        "imgUrl" : "https://mblogthumb-phinf.pstatic.net/20110624_235/itowa2_13088439808451YTUX_JPEG/15.jpg?type=w2",
+        "desc" : "이미지를 보면 성향이 편안하지는 않지만 실제 이 카드의 주인공인 당신은 위계질서를 좋아하고 통치욕과 야망이 크며 의리가 대단한 사람입니다. 욱하는 성격도 때론 있지만 인간의 기본 욕구에 충실한 타입이죠."
+    },
+    {
+        "cardNum" : "16",
+        "cardName" : "THE TOWER",
+        "imgUrl" : "https://mblogthumb-phinf.pstatic.net/20110624_292/itowa2_1308843981039dqLcv_JPEG/16.jpg?type=w2",
+        "desc" : "급격한 변화가 필요한 당신은 도전과 모험을 즐기는 성향이 강하고 겉으로 표출하는 것을 잘하다보니 타인들이 다혈질이라고 말하기도 하죠. 예기치 않은 일에도 당황하지않고 과감히 자신을 변화시키는 카멜레온같은 기질이 강합니다."
+    },
+    {
+        "cardNum" : "17",
+        "cardName" : "THE STAR",
+        "imgUrl" : "https://mblogthumb-phinf.pstatic.net/20110624_34/itowa2_1308843981215K57FM_JPEG/17.jpg?type=w2",
+        "desc" : "건강과 활력이 넘치는 당신은 긍정적 에너지가 넘치고 신념과 자신감이 매우 높아 주변의 사랑을 많이 받는 성향입니다. 호기심도 많고 예술적 끼도 많아 희망이 넘치는 타입입니다."
+    },
+    {
+        "cardNum" : "18",
+        "cardName" : "THE MOON",
+        "imgUrl" : "https://mblogthumb-phinf.pstatic.net/20110624_156/itowa2_1308843981440TCaVN_JPEG/18.jpg?type=w2",
+        "desc" : "달은 여성적 에너지를 상징한다. 예민해서 자칫 우울성향이 나타날 수 있으나 주변을 먼저 챙기는 모성애가 강한 성향이라고 보여집니다. 걱정이 앞서는 타입이지만 돌다리도 두들기는 꼼꼼한 성격이라 실수는 없겠지요."
+    },
+    {
+        "cardNum" : "19",
+        "cardName" : "THE SUN",
+        "imgUrl" : "https://mblogthumb-phinf.pstatic.net/20110624_289/itowa2_1308843981616xa4Iz_JPEG/19.jpg?type=w2",
+        "desc" : "태양은 싱싱한 건강과 활력을 상징하며 매우 긍정을 나타낸다. 이러한 성향의 당신은 무조건 성격좋고 남들도 모두 인정해주는 뒤끝없이 밝은 사람이다. 밝고 명랑한 아이와 같은 성향입니다."
+    },
+    {
+        "cardNum" : "20",
+        "cardName" : "JUDGEMENT",
+        "imgUrl" : "https://mblogthumb-phinf.pstatic.net/20110624_35/itowa2_1308843981808d3xIV_JPEG/20.jpg?type=w2",
+        "desc" : "강력한 재탄생의 이미지를 가진 당신은 합리적인 사고를 가진 성향으로 대단한 용기와 끈기를 가지고 있으며 때론 어른도 가르칠 정도의 도덕성을 보입니다."
+    },
+    {
+        "cardNum" : "21",
+        "cardName" : "THE WORLD",
+        "imgUrl" : "https://mblogthumb-phinf.pstatic.net/20110624_52/itowa2_1308843982224TaNr4_JPEG/21.jpg?type=w2",
+        "desc" : "마음이 넓고 노력형인 당신은 자기만의 뚜렷한 세계가 있고 스케일이 큽니다. 혼자 이루려는 성향이 매우 강해 내 사람이다 싶으면 좋아라 하지만 나머지는 관심을 두지 않는 하고 싶은것과 하기 싫은것의 차이가 심한 성격입니다."
+    }
 ]
